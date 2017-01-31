5340364a12e3940668134e871e7adcafb334b27c
zhang165 <zhang165@gmail.com>
2017-01-31 11:04:10 -0800

    Moved constants

--- a3/main.py
+++ a3/main.py
@@ -6,8 +6,11 @@ import ncc
 
 # define constants
 pyramid_reduction = 0.75
+pyramid_min_size = 5
 template_width = 15
 match_threshold = 0.65
+image_loc = 'C:\UBC\CPSC425\\a3\\faces\\sports.jpg'
+template_loc = 'C:\UBC\CPSC425\\a3\\faces\\template.jpg'
 
 # define functions
 def makePyramid(image, minsize):
@@ -54,23 +57,26 @@ def findTemplate(pyramid, template, threshold):
 		for j in range(len(corr)): # iterate to find positions 
 			for k in range(len(corr[j])):
 				if(corr[j][k] > threshold):	# we've found a match
-					ratio = 1/(pyramid_reduction**i)
-					p = ratio*k -a/2 # find top left corner
-					q = ratio*j -b/2
-					draw.line((p,q,p+a,q),fill="red",width=2) #draw a box
-					draw.line((p,q,p,q+b),fill="red",width=2)
-					draw.line((p+a,q,p+a,q+b),fill="red",width=2)
-					draw.line((p,q+b,p+a,q+b),fill="red",width=2)
+					ratio = 1/(pyramid_reduction**i) # calculate ratios and offsets
+					a_offset = ratio*a; 
+					b_offset = ratio*b;
+					p = ratio*k -a_offset/2 # find the center
+					q = ratio*j -b_offset/2
+					
+					draw.line((p,q,p+a_offset,q),fill="red",width=2) #draw a box
+					draw.line((p,q,p,q+b_offset),fill="red",width=2)
+					draw.line((p+a_offset,q,p+a_offset,q+b_offset),fill="red",width=2)
+					draw.line((p,q+b_offset,p+a_offset,q+b_offset),fill="red",width=2)
 	del draw
 	return marked
 
 
 # Run our scripts
-im = Image.open('G:\UBC\CPSC425\\assignments\\a3\\faces\\judybats.jpg')
-pyramid = makePyramid(im, 25) #create pyramid
+im = Image.open(image_loc)
+pyramid = makePyramid(im, pyramid_min_size) #create pyramid
 #showPyramid(pyramid)
 
-template = Image.open('G:\UBC\CPSC425\\assignments\\a3\\faces\\template.jpg')
+template = Image.open(template_loc)
 template = resize(template, template_width) # resize template
 
 marked = findTemplate(pyramid,template, match_threshold)
