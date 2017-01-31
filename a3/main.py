ab83970c9ee1d9960e6be186aed460de0da58945
zhang165 <zhang165@gmail.com>
2017-01-30 22:24:03 -0800

    Working face matching code

--- a3/main.py
+++ a3/main.py
@@ -4,62 +4,76 @@ import math
 from scipy import signal
 import ncc
 
+# define constants
+pyramid_reduction = 0.75
+template_width = 15
+match_threshold = 0.65
+
+# define functions
 def makePyramid(image, minsize):
 	[x,y] = image.size
 	res = []
 	while (x > minsize and y > minsize):
 		image_next = image.resize((x,y), Image.BICUBIC)
-		x = int(x*0.75)
-		y = int(y*0.75)
+		x = int(x*pyramid_reduction)
+		y = int(y*pyramid_reduction)
 		res.append(image_next)
 	return res
 
 def showPyramid(pyramid):
 	[x,y] = pyramid[0].size
-	image = Image.new("L", (x*3,y), "white");
 	
-	offset = 0
+	length = 0	# find the length
 	for i in range(len(pyramid)):
+		length = length + pyramid[i].size[0]
+	
+	image = Image.new("L", (length,y), "white"); # generate white image
+
+	offset = 0
+	for i in range(len(pyramid)): # paste each image
 		image.paste(pyramid[i],(offset,y-pyramid[i].size[1]))
 		offset = offset + pyramid[i].size[0]
 	image.show()
 	return image
 
-def boxfilter(n):
-	assert n%2 is not 0, "Dimension must be odd"
-	return np.full((n,n),1.0/(n*n), dtype = float)
+def resize(template, width=15):
+	[x,y] = template.size
+	ratio = (width/float(x))
+	template = template.resize((width, int(y*ratio)), Image.BICUBIC) #resize image
+	return template
 
-def gauss1d(sigma=1):
-	# generate n, ensure n is odd
-	n = np.ceil(6*sigma)
-	n = n+1 if n%2 == 0 else n
+def findTemplate(pyramid, template, threshold):
+	[a,b] = template.size
 
-	i = np.arange(-np.floor(n/2),np.floor(n/2)+1,1) # generate the range
-	gauss = np.vectorize(lambda x: np.exp((-x**2)/(2*sigma**2))) # mapping function
-	i = gauss(i) # apply the map
-	return 1/sum(i)*i # return normalized
+	marked = pyramid[0].convert('RGB') # the marked image to return
+	draw = ImageDraw.Draw(marked) 
 
-def gauss2d(sigma=1):
-	i = gauss1d(sigma) # generate 1d guass
-	i = i[np.newaxis] # generate 2d 
-	i_t = np.transpose(i) # create transpose
+	for i in range(len(pyramid)): # for every image
+		[x,y] = pyramid[i].size
+		corr = ncc.normxcorr2D(pyramid[i],template)	
 
-	return signal.convolve2d(i, i_t) # convolve i with i'
+		for j in range(len(corr)):
+			for k in range(len(corr[j])):
+				if(corr[j][k] > threshold):	# we've found a match
+					ratio = 1/(pyramid_reduction**i)
+					p = ratio*k -a/2 # find corner
+					q = ratio*j -b/2
+					draw.line((p,q,p+a,q),fill="red",width=2)
+					draw.line((p,q,p,q+b),fill="red",width=2)
+					draw.line((p+a,q,p+a,q+b),fill="red",width=2)
+					draw.line((p,q+b,p+a,q+b),fill="red",width=2)
+	del draw
+	return marked
 
-def gaussconvolve2d(array,sigma):
-	f = gauss2d(sigma) # generate 2d filter
-	array = signal.convolve2d(array,f,'same') # convolve with 2d filter of same size
-	return array
 
-im = Image.open('G:\UBC\CPSC425\\assignments\\a3\\faces\\template.jpg')
-pyramid = makePyramid(im, 10)
-showPyramid(pyramid)
 
-# im = Image.open('C:\UBC\CPSC425\\a2\\teemo.png')
-# im = im.convert('L')
-# im.save('C:\UBC\CPSC425\\a2\\gray_teemo.png','PNG')
+# Run our script	
+im = Image.open('G:\UBC\CPSC425\\assignments\\a3\\faces\\judybats.jpg')
+pyramid = makePyramid(im, 25) #create pyramid
+#showPyramid(pyramid)
 
-# im_array = np.asarray(im)
-# im_array = gaussconvolve2d(im_array,3)
-# im2 = Image.fromarray(im_array.astype('uint8'))
-# im2.save('C:\UBC\CPSC425\\a2\\convolved_teemo.png','PNG')
+template = Image.open('G:\UBC\CPSC425\\assignments\\a3\\faces\\template.jpg')
+template = resize(template, template_width) # resize template
+
+marked = findTemplate(pyramid,template, match_threshold)
+marked.show()
